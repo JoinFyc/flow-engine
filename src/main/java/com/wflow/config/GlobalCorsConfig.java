553087e79f4f257f9cd8d860ce64135df2cd4ef5
JoinFyc <fyc139050@163.com>
2024-09-03 08:48:42 +0800

    test

--- src/main/java/com/wflow/config/GlobalCorsConfig.java
+++ src/main/java/com/wflow/config/GlobalCorsConfig.java
@@ -27,16 +27,7 @@ public class GlobalCorsConfig {
 
     @Bean
     public FilterRegistrationBean filterRegistrationBean() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        //1.允许任何来源
-        corsConfiguration.addAllowedOrigin(webDomain);
-        corsConfiguration.addAllowedOriginPattern("*");
-        //2.允许任何请求头
-        corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
-        //3.允许任何方法
-        corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
-        //4.允许凭证
-        corsConfiguration.setAllowCredentials(true);
+        final CorsConfiguration corsConfiguration = getCorsConfiguration();
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
@@ -45,6 +36,21 @@ public class GlobalCorsConfig {
         FilterRegistrationBean<CorsFilter> filterRegistrationBean=new FilterRegistrationBean<>(corsFilter);
         filterRegistrationBean.setOrder(-101);
         return filterRegistrationBean;
+    }
+
+    private CorsConfiguration getCorsConfiguration() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //1.允许任何来源
+        corsConfiguration.addAllowedOrigin(webDomain);
+        corsConfiguration.addAllowedOrigin("http://192.168.68.89:5173");
+        corsConfiguration.addAllowedOriginPattern("*");
+        //2.允许任何请求头
+        corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
+        //3.允许任何方法
+        corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
+        //4.允许凭证
+        corsConfiguration.setAllowCredentials(true);
+        return corsConfiguration;
     }
 
 }
